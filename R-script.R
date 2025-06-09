ff2ade316a1a5eebc6ca2f0a79c1ec5490201b77
drjoseluisheram <drjoseluisheram@gmail.com>
2025-06-09 14:29:06 +0100

    Initial commit

--- R-script.R
+++ R-script.R
@@ -22,18 +22,20 @@ usethis::create_github_token()
 
 #3 Se abrirá una ventana de GitHub en la que podrás generar y copiar el token. Ejecuta la siguiente función, y cuando la consola te lo indique, pega el token que copiaste:
 #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
-#gitcreds_get()
-#gitcreds_set()
+gitcreds_get()
+gitcreds_set()
 
 
 #4. Confirmar que está funcionando bien:
 usethis::git_sitrep()
 
 #Crear un repositorio local
+usethis::use_git()
+
 
 #Subir el repositorio local a GitHub - Una vez creado, no es necesario hacerlo todo el tiempo
 usethis::use_github()
- usethis::use_git_remote("origin", url = "https://github.com/drjoseluisheram/Quarto-DrJoseLuisHeram.git", overwrite = TRUE)
+# usethis::use_git_remote("origin", url = "https://github.com/drjoseluisheram/Quarto-DrJoseLuisHeram.git", overwrite = TRUE)
  
 usethis::use_readme()
 
